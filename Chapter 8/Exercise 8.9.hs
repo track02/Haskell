262a45916ec6955b628a4be6af1a9fa6b2e1e9f3
Alex Clarke <a.clarke@runbox.com>
2018-01-29 12:16:42 +0000

    Formatting on 8.9

--- Chapter 8/Exercise 8.9.hs
+++ Chapter 8/Exercise 8.9.hs
@@ -8,8 +8,8 @@ data Op = EVAL_ADD Expr | EVAL_MULT Expr | ADD Int | MULT Int
 
 eval :: Expr -> Cont -> Int  -- Evaluates an expression and updates the stack
 eval (Val n)    c = exec c n  -- If value expression is reached then there's nothing else to evaluate - start working back down stack executing the stored up commands 
-eval (Add x y)  c = eval x (EVAL_ADD y  : c) -- If Add expression reached, split into two evaluations for LHS and RHS, 
-eval (Mult x y) c = eval x (EVAL_MULT y : c) -- If Mult expression reached, split into two evaluations for LHS and RHS, a              
+eval (Add x y)  c = eval x (EVAL_ADD y  : c) -- If Add expression reached, split into two evaluations for LHS and RHS 
+eval (Mult x y) c = eval x (EVAL_MULT y : c) -- If Mult expression reached, split into two evaluations for LHS and RHS             
 
 
 exec :: Cont -> Int -> Int  -- Pops the topmost command from the stack and executes it 
@@ -22,6 +22,26 @@ exec (MULT n : c) m = exec c (n*m) -- Mult command, multiply value with register
 value :: Expr -> Int
 value e = eval e []
 
+-- Given an Add expression 
+-- Split it into LHS and RHS 
+-- Repeatedbly evaluate the LHS, Storing an Evaluate Command on the Stack for the each RHS of any encountered Expressions
+-- When a value is reached, no further evaluation can occur this value is stored in the stack 
+-- Then start working back down the stack (or up the expression) evaluating the nearest RHS expression
+-- Once this RHS expression is evaluated to a value this value is added to the stored LHS value 
+-- The process then ends here or repeats if this was the LHS of a larger enclosing expression.
+
+-- Add (Add Val 1 Val 2) (Val 3)
+-- 1) Eval Add (Add Val 1 Val 2) Val 3
+-- 2) Eval (Add Val 1 Val 2)     Stack: EVAL Val 3
+-- 3) Eval Val 1                 Stack: EVAL Val 2, EVAL Val 3    
+-- 4)      Exec (EVAL Val 2) 1   Stack: ADD 1, EVAL Val 3
+-- 5) Eval Val 2 
+-- 6)      Exec (ADD 1) 2        Stack: EVAL Val 3
+-- 7)      Exec (EVAL Val 3) 3   Stack: ADD 3
+-- 8) Eval Val 3                 Stack: ADD 3
+-- 9)      Exec (ADD 3) 3        Stack: []
+-- 10)     Exec [] 6
+-- 11)     6
 
 
 -- e1 = Add (Val 5) (Val 6)
