b932edc6302c32ef250e87090593622bc6987533
track02 <a.clarke@runbox.com>
2018-02-05 19:49:35 +0000

    Ex 1 note

--- Chapter 10/Exercise 10.1.hs
+++ Chapter 10/Exercise 10.1.hs
@@ -1,2 +1,7 @@
 Redefine putStr :: String -> IO () using a list comprehension and the library function
 sequence_ :: [IO a] -> IO ()
+
+putStr :: String -> IO ()
+putStr [] = return ()
+putStr (x:xs) = do putChar x
+                   putStr xs
