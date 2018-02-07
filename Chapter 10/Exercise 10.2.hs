226cdb0cf5d72e1f88d2909db047a2ed5a0cfda3
Alex Clarke <a.clarke@runbox.com>
2018-02-07 10:32:27 +0000

    Chapter 10 - Exercise 10.2 and 10.3

--- Chapter 10/Exercise 10.2.hs
+++ Chapter 10/Exercise 10.2.hs
@@ -1,7 +1,34 @@
-Using recursion define a version of
+-- Using recursion define a version of
+-- putBoard :: Board -> IO ()
+-- that displays nim boards of any size rather than being specific to boards of 5 rows
+
+-- Hint - first define a function that takes the current row number as an additional argument
+
+-- Original definitions
+type Board = [Int]
+
+putRow :: Int -> Int -> IO ()
+putRow row num = do putStr (show row)
+                    putStr ": "
+                    putStrLn (concat (replicate num "* "))
 
 putBoard :: Board -> IO ()
+putBoard [a,b,c,d,e] = do putRow 1 a
+                          putRow 2 b
+                          putRow 3 c
+                          putRow 4 d
+                          putRow 5 e
+		  				  
 
-that displays nim boards of any size rather than being specific to boards of 5 rows
+-- Alternatively we can iterate over the board 
 
-Hint - first define a function that takes the current row number as an additional argument
+-- We'll use a function which takes a board and row number 
+showBoard :: Board -> Int -> IO () 
+showBoard [x]    n = putRow n x
+showBoard (x:xs) n = do putRow n x
+                        showBoard xs (n + 1)
+
+-- And a function which sets up the first instance 
+putBoard'' :: Board -> IO()
+putBoard'' b = showBoard b 1
+
--- Chapter 10/Exercise 10.3.hs
+++ Chapter 10/Exercise 10.3.hs
@@ -1,2 +1,26 @@
-In a similar manner to exercise 1, redefine the generalised version of putBoard
-using a list comprehension and sequence_
+-- In a similar manner to exercise 1, redefine the generalised version of putBoard
+-- using a list comprehension and sequence_
+
+-- Original definitions
+type Board = [Int]
+
+putRow :: Int -> Int -> IO ()
+putRow row num = do putStr (show row)
+                    putStr ": "
+                    putStrLn (concat (replicate num "* "))
+
+putBoard :: Board -> IO ()
+putBoard [a,b,c,d,e] = do putRow 1 a
+                          putRow 2 b
+                          putRow 3 c
+                          putRow 4 d
+                          putRow 5 e
+
+-- We can use a list comprehension to generate a list of putRow actions
+-- Zipping the board against an infinite list will produce a list of tuples containing (row / num)
+-- We can then draw from this list to create putRow actions 
+-- The sequence_ method can then be used to iterate over this action list and evaluate each putRow action 
+-- This results in the board being displayed to output
+						  
+putBoard' :: Board -> IO ()
+putBoard' xs = sequence_ [putRow r n | (r,n) <- zip [1..] xs] 
