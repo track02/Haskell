4118b679acfd350dfa6303c2302fa213c219890a
Alex Clarke <a.clarke@runbox.com>
2018-02-08 15:59:14 +0000

    Haskel Exercise 4 and 5 Chp 10

--- Chapter 10/Exercise 10.4.hs
+++ Chapter 10/Exercise 10.4.hs
@@ -1,18 +1,43 @@
-Define an action adder :: IO () that reads a given number 
-of integers from the keyboard, one per line and displays their sum
+-- Define an action adder :: IO () that reads a given number 
+-- of integers from the keyboard, one per line and displays their sum
 
-E.g.
+-- E.g.
+--
+-- > adder
+-- How many numbers? 5
+-- 1
+-- 3
+-- 5
+-- 7
+-- 9
+-- The total is 25
 
-> adder
-How many numbers? 5
-1
-3
-5
-7
-9
-The total is 25
+-- Hint start by defining a function that takes the current total 
+-- and how many numbers remain to be read as arguments 
 
-Hint start by defining a function that takes the current total and How
-many numbers remain to be read as arguments 
+-- You will also need to use the library functions read and show 
 
-You will also need to use the library functions read and show 
+import Data.Char
+
+
+-- We'll use a variant of the getDigit function from the nim example
+getDigit :: String -> IO Int 
+getDigit prompt = do putStr prompt
+                     input <- getLine
+                     return (read input :: Int)
+
+adder :: IO ()
+adder = do
+           input <- getDigit "How many numbers? " -- Determine no. of values to add 
+           total <- adder' 0 input -- Evaluate adder using input and hold result in total 
+           putStrLn (show total) -- Use show to get string repr and then display 
+
+adder' :: Int -> Int -> IO Int 
+adder' t n = do 
+                input <- getDigit "> " -- Take input
+                if n == 1 then 
+			       return (t + input)
+			    else
+                   adder' (input + t) (n - 1)
+
+              
--- Chapter 10/Exercise 10.5.hs
+++ Chapter 10/Exercise 10.5.hs
@@ -1,2 +1,22 @@
-Redefine adder using the function sequence :: [IO a] -> IO [a] that performs
-a list of actions and returns a list of the resulting values
+-- Redefine adder using the function sequence :: [IO a] -> IO [a] that performs
+-- a list of actions and returns a list of the resulting values
+
+import Data.Char
+
+
+adderSeq :: IO ()
+adderSeq = do
+           input <- getDigit "How many numbers? " -- Determine no. of values to add 
+           total <- adderSeq' input -- Evaluate adder using input and hold result in total 
+           putStrLn (show (sum total)) -- Use show to get string repr and then display 
+
+adderSeq' :: Int -> IO [Int] 
+adderSeq' n = sequence [(getDigit "> ") | _ <- [1..n]]
+
+
+
+-- We'll use a variant of the getDigit function from the nim example
+getDigit :: String -> IO Int 
+getDigit prompt = do putStr prompt
+                     input <- getLine
+                     return (read input :: Int)
