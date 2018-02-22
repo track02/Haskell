07ce69f55b5e40bf8861a16b9d802b3dbf922c2b
Alex Clarke <a.clarke@runbox.com>
2018-02-22 08:30:07 +0000

    Exercise 11.4 - Part 2

--- Chapter 11/Exercise 11.4.hs
+++ Chapter 11/Exercise 11.4.hs
@@ -12,6 +12,10 @@ import System.IO
 data Player = O | B | X
               deriving (Eq, Ord, Show)
 
+type Grid = [[Player]]
+
+size = 3
+
 -- 1) Let user decide to go first or second 
 -- To achieve this we'll create a function which
 -- prompts the user for their desired turn and
@@ -29,6 +33,45 @@ getNat prompt = do putStr prompt
                       do putStrLn "ERROR: Invalid number"
                          getNat prompt
 
-
 -- 2) Allow the length of a winning line to be modified 
--- 
+-- To do this we need another value to represent
+-- the length of a winning line 
+
+winLength = 2
+
+-- We then need to chop rows / cols / diags into sub-lists 
+-- of this length 
+-- E.g. [[O,X,X],[X,O,O],[B,B,B]
+-- Would become [[O,X],[X,X], [X,O],[O,O], [B,B],[B,B]]
+
+-- We'll make a new procedure to split up a line list 
+chopLines :: [[Player]] -> [[Player]]
+chopLines [] = []
+chopLines (x:xs) = (chop winLength x) ++ chopLines xs 
+
+-- We'll modify chop slightly to take a length then drop only one element 
+chop :: Int -> [a] -> [[a]]
+chop n [] = []
+chop n xs |length xs < n = []  -- Stop when there are no more elements to take 
+          |otherwise = take n xs : chop n (drop (n - 1) xs)
+
+-- Testing 		  
+-- chopLines [[X,O,X],[O,O,X],[B,B,B]]
+-- [[X,O],[O,X],
+--  [O,O],[O,X],
+--  [B,B],[B,B]]
+
+-- And update the wins procedure to split each returned [[Player]] list into futher sublists of length winLength 
+wins :: Player -> Grid -> Bool
+wins p g = any line (rows ++ cols ++ dias)
+           where
+              line = all (== p)
+              rows = chopLines g
+              cols = chopLines $ transpose g
+              dias = chopLines [diag g, diag (map reverse g)]
+
+-- wins O [[X,O,X],[O,O,X],[B,B,B]]
+-- True
+
+diag :: Grid -> [Player]
+diag g = [g !! n !! n | n <- [0..size-1]]
